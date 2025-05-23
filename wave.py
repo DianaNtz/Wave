ef54b152c5a3adb681e536f403d5a90cfb00dae7
Diana Nitzschke <diana.nitzschke@uni-jena.de>
2025-05-23 11:30:06 +0200

    added finite difference derivatives

--- wave.py
+++ wave.py
@@ -25,3 +25,40 @@ t0=0
 tfinal=2/np.sqrt(2)
 dt=(tfinal-t0)/(nt-1)
 t=0
+#finite difference derivatives
+def d2x(D):
+    Dxx=np.zeros((ny,nx), dtype='double')
+    for j in range(0,ny):
+        for i in range(0,nx):
+                 if(i==0):
+                     Dxx[j][0]=(D[j][2]-2*D[j][1]+D[j][0])/(dx**2)
+                 if(i==nx-1):
+                     Dxx[j][nx-1]=(D[j][nx-1]-2*D[j][nx-1-1]+D[j][nx-1-2])/(dx**2)
+                 if(i!=0 and i!=nx-1):
+                     Dxx[j][i]=(D[j][i+1]-2*D[j][i]+D[j][i-1])/(dx**2)
+    return Dxx
+
+def d2y(D):
+    Dyy=np.zeros((ny,nx), dtype='double')
+    for j in range(0,ny):
+        for i in range(0,nx):
+                 if(j==0):
+                     Dyy[j][0]=(D[2][i]-2*D[1][i]+D[0][i])/(dy**2)
+                 if(j==ny-1):
+                     Dyy[ny-1][i]=(D[ny-1][i]-2*D[ny-1-1][i]+D[ny-1-2][i])/(dy**2)
+                 if(j!=0 and j!=ny-1):
+                     Dyy[j][i]=(D[j+1][i]-2*D[j][i]+D[j-1][i])/(dy**2)
+    return Dyy
+
+
+
+un1=np.empty([ny,nx], dtype='double')
+un2=np.empty([ny,nx], dtype='double')
+ua0=np.empty([ny,nx], dtype='double')
+for j in range(0,ny):
+        for i in range(0,nx):
+            un1[j][i]=np.sin(np.pi*x[i])*np.sin(np.pi*y[j])
+            un2[j][i]=np.sin(np.pi*x[i])*np.sin(np.pi*y[j])
+            ua0[j][i]=np.sin(np.pi*x[i])*np.sin(np.pi*y[j])
+
+Y,X= np.meshgrid(y, x)
